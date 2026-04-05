a8d9551b28c3b5669b06e9190c2e414b4219d1cb
개그튼코딩 <hoon62261662@gmail.com>
2026-04-05 19:10:09 +0900

    Add files via upload

---
+++ crawler/scrapers/__init__.py
@@ -0,0 +1,7 @@
+from .contestkorea import ContestKoreaScraper
+from .thinkcontest import ThinkContestScraper
+from .linkareer import LinkareerScraper
+from .wevity import WevityScraper
+from .dacon import DaconScraper
+
+__all__ = ["ContestKoreaScraper", "ThinkContestScraper", "LinkareerScraper", "WevityScraper", "DaconScraper"]
